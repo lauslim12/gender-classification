d25a5183152d35ba28b9c4d5f94c333157d7db23
lauslim12 <nicholasdwiarto@yahoo.com>
2020-07-25 11:49:09 +0700

    Added baseline for the Python script

---
+++ main.py
@@ -0,0 +1,17 @@
+#!usr/bin/env python
+
+# Developer metadata.
+__author__ = "Nicholas Dwiarto W."
+__copyright__ = "Copyright 2020, Nicholas Dwiarto W."
+__credits__ = "Nicholas Dwiarto W."
+
+__license__ = "MIT"
+__version__ = "1.0"
+__maintainer__ = "Nicholas Dwiarto W."
+__status__ = "Prototype"
+
+def main():
+    print('Application in progress!')
+
+if __name__ == "__main__":
+    main()
